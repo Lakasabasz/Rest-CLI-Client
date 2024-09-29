f4644b099348b488f8e877c7be0dc2107abe3a35
Lakasabasz <lakasabasz@gmail.com>
2024-09-29 17:52:42 +0200

    Missing files

---
+++ RestCliClient.Tests/TestConsts.cs
@@ -0,0 +1,51 @@
+namespace RestCliClient.Tests;
+
+static class TestConsts
+{
+    public const string ReadMeJson = 
+"""
+{
+  "variables": {
+    "in": [
+      "token",
+      "username",
+      "password"
+    ],
+    "out": [
+      "user-id",
+      "user-token"
+    ]
+  },
+  "sequence": [
+    {
+      "uri": "https://example.com/api/users",
+      "method": "POST",
+      "options": {
+        "ignore_ssl": false,
+        "timeout": 15
+      },
+      "headers": [
+        {"name": "Authorization", "value": "Bearer $token"},
+        {"name": "Content-Type", "value": "application/json"}
+      ],
+      "body": "{\"username\": \"$username\", \"password\": \"$password\", \"category\": \"$$\"}",
+      "response_operations": [
+        {"variable": "user-guid", "value": "$.user.id"}
+      ]
+    }
+  ]
+}
+""";
+
+    public const string ReadMeMinimalJson =
+"""
+{
+  "sequence": [
+    {
+      "uri": "https://example.com/api/users",
+      "method": "POST"
+    }
+  ]
+}
+""";
+}
